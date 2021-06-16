a46ab70937fe2cb0b0cdeb5012c1dd750f309ae0
Julian Saenze <julian.saenze@comsysto.com>
2021-06-16 12:04:03 +0200

    tidy up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // loadedFeature = 'recipe';
-  // showComponent = true;
-  // inputValue = 123;
 
-  // onNavigate(feature: string) {
-  //   this.loadedFeature = feature;
-  // }
 }
