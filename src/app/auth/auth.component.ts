70e7c671851d4f273f11d0dbefc25444cd47ee4a
Julian Saenze <julian.saenze@comsysto.com>
2021-06-18 07:48:32 +0200

    sending signup request

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,6 @@
 import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { AuthService } from "./auth.service";
 
 @Component({
   selector: 'app-auth',
@@ -8,7 +10,35 @@ import { Component } from "@angular/core";
 export class AuthComponent {
   isLoginMode = true;
 
+  constructor(private authService: AuthService) {}
+
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit(form: NgForm) {
+    //extra check - user can hack and enable button
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+
+    if(this.isLoginMode) {
+      //logging in
+      // ...
+    } else {
+      //signing up
+      //subscribe to the response value of signUp
+      this.authService.signUp(email, password).subscribe(responseData => {
+        console.log(responseData);
+      },
+      error => {
+        console.log(error);
+      }
+      );
+    }
+
+    form.reset();
   }
 }
