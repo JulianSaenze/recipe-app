c8b791320b916f61cb5be9474ad42af257b4f9d6
Julian Saenze <julian.saenze@comsysto.com>
2021-06-21 13:25:01 +0200

    logging out

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from "@angular/core";
 import { catchError, tap } from "rxjs/operators";
 import { BehaviorSubject, throwError } from "rxjs";
 import { User } from "./user.model";
+import { Router } from "@angular/router";
 
 //good practice to define the data you are working with
 export interface AuthResponseData {
@@ -24,7 +25,8 @@ export class AuthService {
   //Difference is that BehaviorSubject gives access to the previous emitted Subject
   user = new BehaviorSubject<User>(null);
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient,
+              private router: Router) {}
   //Look up Endpoint / Request Body Payload / Response Payload at https://firebase.google.com/docs/reference/rest/auth#section-create-email-password for signing up
 
   signUp(email: string, password: string) {
@@ -39,7 +41,7 @@ export class AuthService {
         catchError(this.handleError));
   }
 
-  //... https://firebase.google.com/docs/reference/rest/auth#section-sign-in-email-password for signing/login in
+  //...for signing/login in
   login(email: string, password: string) {
     return this.http.post<AuthResponseData>(
       'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCMB7UyYC1QDLXGCPaUEkp3YcWiyr9iR6g',
@@ -55,6 +57,11 @@ export class AuthService {
         responseData.idToken,
         +responseData.expiresIn)
     }));
+  }
+
+  logout() {
+    this.user.next(null);
+    this.router.navigate(['/auth']);
   }
 
   private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
