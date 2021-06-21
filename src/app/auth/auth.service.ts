02ff5a3d9710da13619f0e2f7cf2c80fd67c987e
Julian Saenze <julian.saenze@comsysto.com>
2021-06-21 13:38:07 +0200

    auto login

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -59,6 +59,28 @@ export class AuthService {
     }));
   }
 
+  autoLogin() {
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExpirationDate: string;
+    } = JSON.parse(localStorage.getItem('userData'));
+    if (!userData) {
+      return;
+    }
+
+    const loadedUser = new User(
+      userData.email,
+      userData.id,
+      userData._token,
+      new Date(userData._tokenExpirationDate));
+
+      if(loadedUser.token) {
+        this.user.next(loadedUser);
+      }
+  }
+
   logout() {
     this.user.next(null);
     this.router.navigate(['/auth']);
@@ -75,6 +97,7 @@ export class AuthService {
       );
       //emit this as the now logged in user
       this.user.next(user);
+      localStorage.setItem('userData', JSON.stringify(user));
   }
 
   private handleError(errorResponse: HttpErrorResponse){
